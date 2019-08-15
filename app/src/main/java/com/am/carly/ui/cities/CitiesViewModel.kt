1b2785ef4083f321a444a90982c9bc611a3d947d
Abed-Murad <Abed.Murad@ProtonMail.com>
2019-08-15 16:33:31 +0300

    Fix a small bug , add a bigger one.

--- app/src/main/java/com/am/carly/ui/cities/CitiesViewModel.kt
+++ app/src/main/java/com/am/carly/ui/cities/CitiesViewModel.kt
@@ -4,11 +4,11 @@ import android.content.Intent
 import android.view.View
 import androidx.lifecycle.ViewModel
 import com.am.carly.data.repository.UserRepository
-import com.am.carly.ui.cars.AddCarActivity
+import com.am.carly.ui.login.StartActivity
 
 class CitiesViewModel(private val userRepository: UserRepository) : ViewModel() {
 
     fun onLogoutBtnClick(view: View) {
-        view.context.startActivity(Intent(view.context, AddCarActivity::class.java))
+        view.context.startActivity(Intent(view.context, StartActivity::class.java))
     }
 }
